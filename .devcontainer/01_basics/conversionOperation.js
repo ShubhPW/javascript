21b715a8f3d902adfacad0db92030383a1d910a0
Shubham Chaudhary <127684274+ShubhPW@users.noreply.github.com>
2023-08-20 18:16:55 +0000

    01_basics

---
+++ .devcontainer/01_basics/conversionOperation.js
@@ -0,0 +1,31 @@
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+// console.log("hello shubham")
+
+// let str1="hello";
+// let str2="shubham";
+
+// let str=str1+str2;
+
+// console.log("2"+2)
+// console.log(1+2+"2")
+// console.log(1+"2"+2)
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+// let gameCounter=100
+// ++gameCounter;
+// console.log(gameCounter);
+
+
+
+
+
+
+
